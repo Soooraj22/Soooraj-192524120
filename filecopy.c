5f51da62a6e4cdee7a7db4e6627ff2c9ea9ae90e
Soooraj22 <thirumalaisoooraj@gmail.com>
2026-04-10 12:13:54 +0530

    Implement file copy functionality in filecopy.c

---
+++ filecopy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+    int src = open("source.txt", O_RDONLY);
+    int dest = open("dest.txt", O_WRONLY | O_CREAT, 0644);
+
+    char buffer[100];
+    int n;
+
+    while ((n = read(src, buffer, sizeof(buffer))) > 0) {
+        write(dest, buffer, n);
+    }
+
+    close(src);
+    close(dest);
+
+    printf("File copied successfully\n");
+    return 0;
+}
