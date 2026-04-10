e548c78ab59161cb4902a26c02919371039b510d
Soooraj22 <thirumalaisoooraj@gmail.com>
2026-04-10 12:14:51 +0530

    Create preemptive_priority.c

---
+++ preemptive_priority.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Preemptive Priority Scheduling (Concept demo)\n");
+    printf("Higher priority executes first, can interrupt others.\n");
+    return 0;
+} 
