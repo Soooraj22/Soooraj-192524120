3e28c309caffecd71f12d298da34ca3b5d52f12d
Soooraj22 <thirumalaisoooraj@gmail.com>
2026-04-15 12:55:57 +0530

    Add multithreading example using pthreads

---
+++ Multithreading.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void* func(void* arg) {
+    printf("Thread is running\n");
+    return NULL;
+}
+
+int main() {
+    pthread_t t1;
+
+    pthread_create(&t1, NULL, func, NULL);
+    pthread_join(t1, NULL);
+
+    printf("Main thread ends\n");
+    return 0;
+}
